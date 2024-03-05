77e9c7fdc68bfdcbc3c0a5c749193234dbc3913d
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-03-04 21:30:43 -0500

    Simplify host page

--- src/app/host/page.tsx
+++ src/app/host/page.tsx
@@ -1,16 +1,21 @@
+import { createRoom } from "@/services/roomApi.service";
 import { Logo } from "@/stories/components/Logo/Logo";
+import { UserList } from "@/stories/components/UserList/UserList";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-    // TODO send api call to get room code
-
-    const room = await fetch("http://localhost:4000/rooms", {
-        method: "POST",
-    }).then((resp) => resp.json());
+export default async function Host() {
+    await createRoom().then((room) => redirect(`/host/${room.roomcode}`));
 
     return (
-        <main className="p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="absolute top-0 left-0 p-4">
+                <div className="p-4 rounded shadow bg-neutral-100 text-lg uppercase"></div>
+            </div>
             <div className="absolute top-0 right-0 p-4">
                 <Logo>Dilemma</Logo>
+            </div>
+            <div>
+                <UserList users={[]} />
             </div>
         </main>
     );
