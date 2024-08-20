81cafdc3007df869231df9f62a7bcc940a7fbd12
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-08-19 22:50:54 -0400

    Add to models

--- src/models/room.model.ts
+++ src/models/room.model.ts
@@ -1,6 +1,18 @@
 import { User } from "./user.model";
 
+enum ROOM_STATE {
+    LOBBY,
+    BEFORE_FIRST_CHALLENGE,
+    FIRST_CHALLENGE,
+    BEFORE_SECOND_CHALLENGE,
+    SECOND_CHALLENGE,
+    BEFORE_VOTING,
+    VOTING,
+    VOTING_RESULTS,
+}
+
 export type Room = {
     roomcode: string;
+    state: string; // TODO enum to tract if in challenge, voting, etc
     users: User[];
 };
